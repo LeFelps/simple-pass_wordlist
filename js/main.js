4044791498515c050d4375cdf778a5822247ba35
LeFelps <LeFelps@hotmail.com>
2021-04-06 01:03:52 -0300

    design fixes and base form structure

--- js/main.js
+++ js/main.js
@@ -2,5 +2,32 @@ var app = new Vue({
     el: '#app',
     data: {
       page : 'home',
-    }
+      birthday : '',
+      names: [{ name: "" }],
+      // '\' ?
+      specCharacters : [' ','!','”','#','$','%','&','’','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~']
+    },
+
+    methods: {
+      addName(fieldType) {
+        fieldType.push({ value: "" });
+      },
+
+      removeName(index, fieldType) {
+        fieldType.splice(index, 1);
+      },
+
+      generatePasswords() {
+        if (this.birthday == ''){
+          console.log('Erro');
+          //TODO Create alert element onscreen
+        }else{
+          // yyyy-mm-dd
+          var dates = this.birthday.split("-")
+          var improvedDates = []
+          
+
+        }
+      },
+    },
   })
