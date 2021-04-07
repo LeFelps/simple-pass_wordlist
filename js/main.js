37161ace2ae6aed6b861b9889b10904f70452221
LeFelps <LeFelps@hotmail.com>
2021-04-06 22:46:03 -0300

    general improvements and corrections

--- js/main.js
+++ js/main.js
@@ -4,22 +4,35 @@ var app = new Vue({
       page : 'home',
       birthday : '',
       names: [{ name: "" }],
-      // '\' ?
-      specCharacters : [' ','!','”','#','$','%','&','’','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~']
+      specCharacters : [' ','!','”','#','$','%','&','’','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','\\','^','_','`','{','|','}','~']
     },
 
     methods: {
+
+      //method for adding a new name input field
       addName(fieldType) {
         fieldType.push({ value: "" });
       },
 
+      //method for removing a selected name input field
       removeName(index, fieldType) {
         fieldType.splice(index, 1);
       },
 
       generatePasswords() {
 
-        if (this.birthday == ''){
+        // boolean variable for checking blank name inputs
+        var blankName
+
+        // loop structure that goes through each name and checks if its empty
+        this.names.forEach(element => {
+          if (element.name == ''){
+            blankName = true
+          }
+        });
+
+        // checking for empty required inputs
+        if (this.birthday == '' || blankName){
           console.log('Erro');
           //TODO Create alert element onscreen
         }else{
@@ -36,30 +49,27 @@ var app = new Vue({
           var passwords = ''
 
 
-          // loop structure for building improvedDates array
+          // loop structure for building improvedDates array with every combination of year, month and day with 'yyyy' and 'YY' formats
           for (var i = 0 ; i < 2; i++){
             for (var a = 0 ; a < dates.length ; a++){
               for (var b = 0 ; b < dates.length ; b++){
                 for (var c = 0 ; c < dates.length ; c++){
-                  if (a != b && a != c && b != c){
+                  if (a != b || a != c || b != c){
                     improvedDates.push(dates[a]+dates[b]+dates[c])
                   }
                 }
               }
             }
+
+            // structure to be run if year is still on 'yyyy' format
             if(dates[0].length == 4){
-              for (var a = 0; a < dates.length; a++) {
-                for (var b = 0; b < dates.length; b++) {
-                  if ( a != b){
-                    improvedDates.push(dates[a]+dates[b])
-                  }
-                }
-              }
-              dates[0] = dates[0].substring(2)
-              for (var a = 0; a < dates.length; a++) {
-                for (var b = 0; b < dates.length; b++) {
-                  if ( a != b){
-                    improvedDates.push(dates[a]+dates[b])
+              for (var i = 0 ; i < 2; i++){
+                if (i == 0){dates[0] = dates[0].substring(2)}
+                for (var a = 0; a < dates.length; a++) {
+                  for (var b = 0; b < dates.length; b++) {
+                    if ( a != b){
+                      improvedDates.push(dates[a]+dates[b])
+                    }
                   }
                 }
               }
@@ -85,8 +95,10 @@ var app = new Vue({
             }
           }
           
+          // create single string from passwords array, separated by line breaks
           passwords = passwordsArray.join("\n")
 
+          // saving the passwords string as ma txt file
           var blob = new Blob([passwords], {type: "text/plain;charset=utf-8"});
           saveAs(blob, "passwords.txt");
 
@@ -96,7 +108,7 @@ var app = new Vue({
     },
   })
 
-
+  // function for removing duplicate entries used on the improvedDates array
   function removeDuplicates(array){
     var result = new Array();
     for (var a = array.length - 1; a >= 0; a--) {
